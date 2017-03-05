8f3e94f17f0a92ed54015dede8c822efc98dd955
OneHappyStone <ll499625801@gmail.com>
2017-03-05 21:57:49 +0800

    引入turbine-stream

--- microservice-cloud-eureka-turbine/src/main/java/com/onehappystone/eureka/turbine/Application.java
+++ microservice-cloud-eureka-turbine/src/main/java/com/onehappystone/eureka/turbine/Application.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 /**
  * Created by happy on 2017/3/4.
  */
-@EnableTurbine
+@EnableTurbineStream
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableHystrixDashboard
