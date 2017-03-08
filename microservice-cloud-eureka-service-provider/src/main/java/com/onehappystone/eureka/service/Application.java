c30e8fb2ac9f0425067618ac1a1996fe5dd5bb56
OneHappyStone <ll499625801@gmail.com>
2017-03-08 15:58:47 +0800

    更改yml部分配置

--- microservice-cloud-eureka-service-provider/src/main/java/com/onehappystone/eureka/service/Application.java
+++ microservice-cloud-eureka-service-provider/src/main/java/com/onehappystone/eureka/service/Application.java
@@ -2,14 +2,16 @@ package com.onehappystone.eureka.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * Created by happy on 2017/3/3.
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableHystrix
 public class Application {
     public static void main(String[] args) {
--- microservice-cloud-eureka-turbine/src/main/java/com/onehappystone/eureka/turbine/Application.java
+++ microservice-cloud-eureka-turbine/src/main/java/com/onehappystone/eureka/turbine/Application.java
@@ -3,6 +3,7 @@ package com.onehappystone.eureka.turbine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
@@ -10,9 +11,9 @@ import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
  * Created by happy on 2017/3/4.
  */
 @EnableTurbineStream
-@EnableDiscoveryClient
-@SpringBootApplication
+@EnableEurekaClient
 @EnableHystrixDashboard
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
